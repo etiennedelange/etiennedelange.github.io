833aabb5dccdb88119db7d20b4f3fcbe9a687af2
Etienne de Lange <etienne.de.lange1@gmail.com>
2024-06-02 12:51:12 +0200

    chore: Add Phaser game framework and update dependencies

--- src/main.js
+++ src/main.js
@@ -1,11 +1,66 @@
-import kaboom from "kaboom";
+// import kaboom from "kaboom";
 
-const k = kaboom();
+// const k = kaboom({global: false, touchToMouse: true});
 
-k.loadSprite("bean", "sprites/bean.png");
+// k.loadSprite("bean", "sprites/bean.png");
 
-k.add([k.pos(120, 80), k.sprite("bean")]);
+// const player = k.add([
+//   k.pos(120, 80),
+//   k.sprite("bean"),
+//   {speed: 300}
+// ]);
 
-console.log("Hello, Kaboom!!!!");
+// k.onMouseDown(() =>
+//   player.moveTo(k.mousePos(), player.speed)
+// );
 
-k.onClick(() => k.addKaboom(k.mousePos()));
+import Phaser from "phaser";
+
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+};
+
+var platforms;
+
+var game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image("sky", "./assets/sky.png");
+  this.load.image("ground", "./assets/platform.png");
+  this.load.image("star", "./assets/star.png");
+  this.load.image("bomb", "./assets/bomb.png");
+  this.load.spritesheet("dude", "./assets/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+}
+
+var platforms;
+
+function create() {
+  this.add.image(400, 300, "sky");
+
+  platforms = this.physics.add.staticGroup();
+
+  platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+  platforms.create(600, 400, "ground");
+  platforms.create(50, 250, "ground");
+  platforms.create(750, 220, "ground");
+}
+
+function update() {}
